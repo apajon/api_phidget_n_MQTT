94a53e731331a8e2ca7bcd66b21d0a4b61bff733
Adrien Pajon (Cirris) <adrien.pajon@gmail.com>
2020-08-07 11:16:48 -0400

    minor debug

--- api_phidget_RUN.sh
+++ api_phidget_RUN.sh
@@ -2,4 +2,4 @@
 
 lxterminal -t "Run Phidget22 encoder reading script" --working-directory=/home/pi/Documents/api_phidget_n_MQTT/src/ -e ./phidget22GetMeasures.py 
 
-lxterminal -t "Run encoder recording script" --working-directory=/home/pi/Documents/api_phidget_n_MQTT/src/ -e ./pidget22SaveLogMeasures.py
+lxterminal -t "Run encoder recording script" --working-directory=/home/pi/Documents/api_phidget_n_MQTT/src/ -e ./phidget22SaveLogMeasures.py
